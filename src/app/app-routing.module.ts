ed4da2e974903aa512dd99f060c6fc57c5a0764f
Samuel VIDEAU <samuelvideau@yahoo.fr>
2021-10-31 16:14:46 +0100

    feat: auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TokensComponent} from "./pages/tokens/tokens.component";
+import {AuthGuard} from "./core/auth/auth.guard";
+import {AuthComponent} from "./pages/auth/auth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tokens', component: TokensComponent, canActivate: [AuthGuard]},
+  { path: 'auth', component: AuthComponent},
+  { path: '', redirectTo: 'tokens', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { TokensComponent } from './pages/tokens/tokens.component';
+import Web3 from "web3";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    TokensComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Web3],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
