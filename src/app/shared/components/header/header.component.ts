1f922b3890ec47e515fbd5d6f998393a9f57b604
Samuel VIDEAU <samuel.videau@efrei.net>
2021-11-26 11:09:36 +0100

    feat: header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,24 @@ export class HeaderComponent implements OnInit {
   pages = [
     {
       title: 'Tokens',
-      route: 'tokens'
+      route: '/tokens'
+    },
+    {
+      title: 'Roles',
+      route: '/roles'
     }
-  ]
+  ];
 
-  constructor() { }
+  actualRoute = this.router.url;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.actualRoute = event.url;
+        console.log(this.actualRoute);
+      }
+    });
   }
 }
